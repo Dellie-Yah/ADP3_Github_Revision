e891404b8011559d811178a3cd294a67bc4c246a
SiphiweChauque <219084777@mycput.ac.za>
2022-02-06 16:11:41 +0200

    test commit

---
+++ src/test/java/za/ac/cput/domain/calcTest.java
@@ -0,0 +1,26 @@
+package za.ac.cput.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calcTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+     @Test
+    void testIdentity(){
+
+    }
+
+    @Test
+    void testEquality(){
+
+    }
+
+
+    }
+
